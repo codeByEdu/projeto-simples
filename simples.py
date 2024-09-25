95bfc7ede25d5928500f4ae5feb1d006775a72e1
codeByEdu <epereira607@gmail.com>
2024-09-25 00:30:15 -0300

    alteracao 2.0

--- simples.py
+++ simples.py
@@ -3,8 +3,8 @@ from tkinter import messagebox    #importa a biblioteca messagebox
 def hello():    #função que exibe a mensagem
     messagebox.showinfo("Hello", "Hello World")
 root = tk.Tk()    #cria a janela
-root.title("Hello")    #define o título da janela
-root.geometry("200x50")    #define o tamanho da janela
+root.title("Hello Edu")    #define o título da janela
+root.geometry("200x500")    #define o tamanho da janela
 button = tk.Button(root, text="Hello", command=hello)    #cria um botão
 button.pack()    #exibe o botão
 root.mainloop()    #inicia o loop principal
