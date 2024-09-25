f195d8529f28c1cbe9d6094a905c68928b54fd3f
codeByEdu <epereira607@gmail.com>
2024-09-25 00:22:52 -0300

    teste

---
+++ simples.py
@@ -0,0 +1,11 @@
+import tkinter as tk    #importa a biblioteca tkinter
+from tkinter import messagebox    #importa a biblioteca messagebox
+def hello():    #função que exibe a mensagem
+    messagebox.showinfo("Hello", "Hello World")
+root = tk.Tk()    #cria a janela
+root.title("Hello")    #define o título da janela
+root.geometry("200x50")    #define o tamanho da janela
+button = tk.Button(root, text="Hello", command=hello)    #cria um botão
+button.pack()    #exibe o botão
+root.mainloop()    #inicia o loop principal
+#https://pt.stackoverflow.com/questions/456727/como-criar-um-app-simples-com-interface-gr%C3%A1fica-em-pythonc
